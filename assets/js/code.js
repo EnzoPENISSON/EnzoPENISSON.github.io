5ce88f655134e0e140a49f075bf541850e5ebb43
EnzoPENISSON <104147716+EnzoPENISSON@users.noreply.github.com>
2022-06-28 17:32:16 +0200

    Add files via upload

--- assets/js/code.js
+++ assets/js/code.js
@@ -2,3 +2,41 @@
 function frame(loc){
 	document.getElementById('shape').src = loc;
 }
+
+
+function downupP1(){
+	document.getElementById('lefond').style.display = 'block';
+	document.getElementById('Popup').style.display = 'block';
+	document.getElementById('myframe').src = "../src/projet1.html"
+}
+
+
+function downupP2(){
+	document.getElementById('lefond').style.display = 'block';
+	document.getElementById('Popup').style.display = 'block';
+	document.getElementById('myframe').src = "../src/projet2.html"
+}
+
+
+function downupP3(){
+	document.getElementById('lefond').style.display = 'block';
+	document.getElementById('Popup').style.display = 'block';
+	document.getElementById('myframe').src = "../src/projet3.html"
+}
+
+function downupP4(){
+	document.getElementById('lefond').style.display = 'block';
+	document.getElementById('Popup').style.display = 'block';
+	document.getElementById('myframe').src = "../src/projet4.html"
+}
+
+function downupP5(){
+	document.getElementById('lefond').style.display = 'block';
+	document.getElementById('Popup').style.display = 'block';
+	document.getElementById('myframe').src = "../src/projet5.html"
+}
+
+function hidde(){
+	document.getElementById('Popup').style.display = 'none';
+	document.getElementById('lefond').style.display = 'none';
+}
